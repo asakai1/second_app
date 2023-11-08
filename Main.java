2af6194d5adc8fbeb650f7a4410809e96a1c4479
asakai1 <127031557+asakai1@users.noreply.github.com>
2023-11-08 14:07:49 +0000

    Add code 1-2

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 // コード0-1
 public class Main {
-  public static void main (String[] args) {
+  public static void main(String[] args) {
     System.out.println("Hello World");
   }
 }
 
 // コード0-2~5
 public class Main {
-  public static void main (String[] args) {
+  public static void main(String[] args) {
     System.out.println("すがわら");
     System.out.println("31歳です");
     System.out.println("お酒が好きです");
@@ -22,7 +22,7 @@ public class Main {
 
 // コード0-6
 public class Main {
-  public static void main (String[] args) {
+  public static void main(String[] args) {
     System.out.println("すがわら");
     System.out.println("31歳です");
     System.out.println("お酒が好きです");
@@ -34,3 +34,50 @@ public class Main {
   }
 }
 
+// 図1-14
+public class Main{
+  public static void main(String[] args){
+    int version;
+    version = 1 + 5;
+    System.out.println("日記ソフト");
+    System.out.println("ver" + version);
+    System.out.println("まだ開発中です");
+    System.out.println("終了します");
+  }
+}
+
+// コード1-2
+public class Main {
+  public static void main(String[] args){
+    int age; /* intは整数 */
+    age = 30;
+    System.out.println(age);
+  }
+}
+
+/*
+整数
+byte とても小さな整数 ex.byte glasses;
+short 小さな整数 ex.short age;
+int 普通の整数 ex.int salary;
+long 大きな整数 ex.long worldPeople;
+
+小数
+float 少し曖昧でも良い小数 ex.float weight;
+double 普通の小数 ex.double pi; 円周率
+
+真偽値
+boolean trueかfalse ex.boolean isError;
+
+文字
+char １つの文字 ex.char initial;
+
+文字列
+String 文字の並び ex.String name;
+*/
+
+public class Main {
+  public static void main(String[] args){
+
+  }
+}
